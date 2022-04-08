e368455a75764cab0fc30807e7510d98ea04c136
AngelMolSan <46252424+AngelMolSan@users.noreply.github.com>
2022-04-08 13:37:07 +0200

    Delete operarMejora.py

--- operarMejora.py
+++
@@ -1,7 +0,0 @@
-def operar(n1,n2,operador='+',respuesta='El resultado es'):
-    if operar=="+":
-        return respuesta+str(n1+n2)
-    elif operador=="-":
-        return respuesta+str(n1-n2)
-    else:
-        return "Error"
